c1552a785ea974d9330b9de28324ef1f564c6850
Imen Felhi <imene.felhi@eezee-it.com>
2022-09-20 18:46:11 +0000

    [ife/eurocable-23831-v1] - 24831/ Globale korting op sales order

--- addons/eurocable_sale/models/sale_order.py
+++ addons/eurocable_sale/models/sale_order.py
@@ -1,10 +1,34 @@
 # Copyright 2022 Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import models
+from odoo import fields, models, api
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    total_discount = fields.Monetary(
+        compute="_compute_total_discount",
+        readonly=False,
+    )
+    total_prices = fields.Monetary(
+        compute="_compute_total_prices",
+        readonly=False,
+    )
+
+    @api.depends("order_line")
+    def _compute_total_prices(self):
+        total_price = 0
+        for rec in self:
+            for order in rec.order_line:
+                total_price += order.price_unit * order.product_uom_qty
+            rec.total_prices = total_price
+
+    @api.depends("order_line")
+    def _compute_total_discount(self):
+        total_discount = 0
+        for order in self.order_line:
+            total_discount += (order.price_unit * order.product_uom_qty) * (order.discount / 100)
+        self.total_discount = round(total_discount, 2)
 
     def action_confirm(self):
         show_warning = self._context.get('show_warning', False)
