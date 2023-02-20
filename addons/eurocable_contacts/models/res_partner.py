d04205586e2e5719aa94db0bc6ebbe260b075fca
Imen Felhi <imene.felhi@eezee-it.com>
2023-02-20 10:29:49 +0100

    f8

--- addons/eurocable_contacts/models/res_partner.py
+++ addons/eurocable_contacts/models/res_partner.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 # Copyright 2023      Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import models, fields
