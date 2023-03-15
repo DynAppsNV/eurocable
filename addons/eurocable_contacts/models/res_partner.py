3886ebcc686106b95ca736a42688cdd68ed13e06
Keval <keval.mehta@eezee-it.com>
2023-03-15 15:43:36 +0530

    [ADD] Security group for partner

--- addons/eurocable_contacts/models/res_partner.py
+++ addons/eurocable_contacts/models/res_partner.py
@@ -1,6 +1,9 @@
-# Copyright 2023      Eezee-IT (<http://www.eezee-it.com>)
+# Copyright 2023
+# Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import models, fields
+
+from odoo import models, fields, _
+from odoo.exceptions import UserError
 
 
 class ResPartner(models.Model):
@@ -8,3 +11,13 @@ class ResPartner(models.Model):
 
     partner_to = fields.Many2one('res.partner',
                                  help="partner used for payment reminder")
+
+    def write(self, vals):
+        if self.user_has_groups('eurocable_contacts.group_user_noedit'):
+            raise UserError(_("You do not have access to edit the user details!"))
+        return super().write(vals)
+
+    def unlink(self):
+        if self.user_has_groups('eurocable_contacts.group_user_noedit'):
+            raise UserError(_("You do not have access to delete the user!"))
+        return super().unlink()
