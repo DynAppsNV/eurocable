196aa4b30626f730134bc3128aadb438f8b49826
Keval <keval.mehta@eezee-it.com>
2023-03-24 18:27:59 +0530

    [UPDATE] Version of module

--- addons/eurocable_purchase/__manifest__.py
+++ addons/eurocable_purchase/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Eurocable Purchase',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.1',
     'author': 'Eezee-It',
     'category': 'Sale',
     'license': 'LGPL-3',
