94495b21271489d22bfe0d0d723fb2dae2c89e76
Keval <keval.mehta@eezee-it.com>
2023-03-24 14:06:18 +0530

    [ADD] Field add in contact

--- addons/eurocable_sale/models/res_partner.py
+++ addons/eurocable_sale/models/res_partner.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Eezee-IT (<http://www.eezee-it.com>)
+# Copyright 2023 Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import models, fields
 
@@ -9,3 +9,5 @@ class ResPartner(models.Model):
     email = fields.Char(required=True)
     ref = fields.Char()
     is_not_vat = fields.Boolean(string="Not VAT Obligated")
+    send_so_to_inv = fields.Boolean('Send SO info with invoice', default=False,
+                                    copy=False)
