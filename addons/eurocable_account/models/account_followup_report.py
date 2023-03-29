53f46d33ada8a65b8020559631e108e8ea0d74da
Keval <keval.mehta@eezee-it.com>
2023-03-29 14:58:06 +0530

    [UPDATE] Remove unused code

--- addons/eurocable_account/models/account_followup_report.py
+++ addons/eurocable_account/models/account_followup_report.py
@@ -41,20 +41,6 @@ class AccountFollowupReport(models.AbstractModel):
                                'email_to': partner.partner_to.email or email}
                 self.env['mail.mail'].sudo().create(mail_values).send()
 
-            # template.with_context(lang=partner.lang).send_mail(self, force_send=True)
-            # # When printing we need te replace the \n of the summary by <br /> tags
-            # body_html = self.with_context(print_mode=True, mail=True).get_html(options)
-            # body_html = body_html.replace('o_account_reports_edit_summary_pencil', 'o_account_reports_edit_summary_pencil d-none')
-            # start_index = body_html.find('<span>', body_html.find('<div class="o_account_reports_summary">'))
-            # end_index = start_index > -1 and body_html.find('</span>', start_index) or -1
-            # if end_index > -1:
-            #     replaced_msg = body_html[start_index:end_index].replace('\n', '')
-            #     body_html = body_html[:start_index] + replaced_msg + body_html[end_index:]
-            # partner.with_context(mail_post_autofollow=True, lang=partner.lang or self.env.user.lang).message_post(
-            #     partner_ids=[invoice_partner.id], body=body_html, subject=self._get_report_manager(options).email_subject,
-            #     subtype_id=self.env.ref('mail.mt_note').id, model_description=_('payment reminder'),
-            #     email_layout_xmlid='mail.mail_notification_light',
-            #     attachment_ids=partner.followup_level.join_invoices and partner.unpaid_invoices.message_main_attachment_id.ids or [], )
             return True
         raise UserError(_('Could not send mail to partner %s because it does '
                           'not have any email address '
