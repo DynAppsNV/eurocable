aa05f7b1d98c7966fcbb7b9bdbe59059e12ce914
Keval <keval.mehta@eezee-it.com>
2023-03-30 12:13:53 +0530

    [ADD] Commets added

--- addons/eurocable_account/wizard/account_invoice_send.py
+++ addons/eurocable_account/wizard/account_invoice_send.py
@@ -8,11 +8,16 @@ from odoo import models, api
 class AccountInvoiceSend(models.TransientModel):
     _inherit = 'account.invoice.send'
 
+    # Over-ride function for adding context while template changes
+    # Use context in mail.compose.message to add attachment
     @api.onchange('template_id')
     def onchange_template_id(self):
         for wizard in self:
             if wizard.composer_id:
+
                 ctx = dict(self._context)
+                # Only work when there is not mass-mail
+                # Also customer have checkbox checked
                 if not len(wizard.invoice_ids) > 1:
                     if wizard.invoice_ids.partner_id.send_so_to_inv:
                         ctx.update({'invoice_sale_attachment':
@@ -20,14 +25,21 @@ class AccountInvoiceSend(models.TransientModel):
 
                 wizard.composer_id.template_id = wizard.template_id.id
                 wizard._compute_composition_mode()
+
+                # Call method via context
                 wizard.composer_id.with_context(ctx).\
                     _onchange_template_id_wrapper()
 
+    # Over-ride function for adding context while mail checkbox change
+    # Used context to add attachment from mail.compose.message
     @api.onchange('is_email')
     def onchange_is_email(self):
         if self.is_email:
             res_ids = self._context.get('active_ids')
+
             ctx = dict(self._context)
+            # Only work when there is not mass-mail
+            # Also customer have checkbox checked
             if not len(self.invoice_ids) > 1:
                 if self.invoice_ids.partner_id.send_so_to_inv:
                     ctx.update({'invoice_sale_attachment':
@@ -43,4 +55,6 @@ class AccountInvoiceSend(models.TransientModel):
                     if len(res_ids) == 1 else 'mass_mail'
                 self.composer_id.template_id = self.template_id.id
                 self._compute_composition_mode()
+
+            # Call method via context
             self.composer_id.with_context(ctx)._onchange_template_id_wrapper()
--- addons/eurocable_account/wizard/mail_compose_message.py
+++ addons/eurocable_account/wizard/mail_compose_message.py
@@ -8,6 +8,9 @@ from odoo import models, api
 class MailComposer(models.TransientModel):
     _inherit = 'mail.compose.message'
 
+    # Over-ride function to add sales attachment in existing values
+    # Based on context do the changes
+    # Return : If conditions full-filled, add the attachment
     @api.onchange('template_id')
     def _onchange_template_id_wrapper(self):
         self.ensure_one()
@@ -16,16 +19,20 @@ class MailComposer(models.TransientModel):
                                             self.res_id)['value']
 
         invoice = self._context.get('invoice_sale_attachment')
+        # Based on context found, do the process
         if invoice:
             sale_attachment = False
             if invoice.invoice_line_ids and \
                     invoice.invoice_line_ids.sale_line_ids:
+                # One invoice will be linked to one sale order
                 sale_order = invoice.invoice_line_ids.\
                     sale_line_ids.mapped('order_id')
                 if sale_order:
+                    # Take the 1st attachment from linked sale order
                     sale_attachment = self.env['ir.attachment'].search([
                         ('res_model', '=', 'sale.order'),
                         ('res_id', '=', sale_order.id)])[0]
+            # Add attachment to existing attachments
             values['attachment_ids'] = [(6, 0,
                                          (values.get('attachment_ids')[0][-1]
                                           + [sale_attachment.id]))]
