0447d6c6237c45fbf88535fbe676c4faf050835b
Keval <keval.mehta@eezee-it.com>
2023-05-09 14:33:30 +0530

    [FIX] Pep8

--- addons/eurocable_account/models/account_move.py
+++ addons/eurocable_account/models/account_move.py
@@ -20,20 +20,34 @@ class AccountMove(models.Model):
         excluded_move_ids = []
 
         if self._context.get('suspense_moves_mode'):
-            excluded_move_ids = AccountMoveLine.search(AccountMoveLine._get_suspense_moves_domain() + [('move_id', 'in', self.ids)]).mapped('move_id').ids
+            excluded_move_ids = AccountMoveLine.\
+                search(AccountMoveLine._get_suspense_moves_domain() +
+                       [('move_id', 'in', self.ids)]).mapped('move_id').ids
 
         for move in self:
-            if move in move.line_ids.mapped('full_reconcile_id.exchange_move_id'):
-                raise UserError(_('You cannot reset to draft an exchange difference journal entry.'))
-            if move.tax_cash_basis_rec_id or move.tax_cash_basis_origin_move_id:
-                # If the reconciliation was undone, move.tax_cash_basis_rec_id will be empty;
-                # but we still don't want to allow setting the caba entry to draft
-                # (it'll have been reversed automatically, so no manual intervention is required),
-                # so we also check tax_cash_basis_origin_move_id, which stays unchanged
-                # (we need both, as tax_cash_basis_origin_move_id did not exist in older versions).
-                raise UserError(_('You cannot reset to draft a tax cash basis journal entry.'))
-            if move.restrict_mode_hash_table and move.state == 'posted' and move.id not in excluded_move_ids:
-                raise UserError(_('You cannot modify a posted entry of this journal because it is in strict mode.'))
+            if move in move.line_ids.\
+                    mapped('full_reconcile_id.exchange_move_id'):
+                raise UserError(_('You cannot reset to '
+                                  'draft an exchange difference journal '
+                                  'entry.'))
+            if move.tax_cash_basis_rec_id or\
+                    move.tax_cash_basis_origin_move_id:
+                # If the reconciliation was undone,
+                # move.tax_cash_basis_rec_id will be empty;
+                # but we still don't want to allow setting the caba entry
+                # to draft
+                # (it'll have been reversed automatically, so no manual
+                # intervention is required),
+                # so we also check tax_cash_basis_origin_move_id, which
+                # stays unchanged
+                # (we need both, as tax_cash_basis_origin_move_id did not
+                # exist in older versions).
+                raise UserError(_('You cannot reset to draft a tax cash'
+                                  ' basis journal entry.'))
+            if move.restrict_mode_hash_table and move.state == 'posted'\
+                    and move.id not in excluded_move_ids:
+                raise UserError(_('You cannot modify a posted entry of this'
+                                  ' journal because it is in strict mode.'))
             # We remove all the analytics entries for this journal
             move.mapped('line_ids.analytic_line_ids').unlink()
 
