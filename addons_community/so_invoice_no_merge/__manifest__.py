2374fdba0e9fd0d57a634597e0c7102da8c19845
Keval <keval.mehta@eezee-it.com>
2023-05-17 16:41:04 +0530

    [ADD] Merge invoice module added

---
+++ addons_community/so_invoice_no_merge/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SW - SO Invoicing No Merge",
+    'summary': "Choose whether to merge multiple sale orders into one invoice or invoice them separately.",
+    'description': """
+        This module grants the user the choice to whether consolidate multiple sales orders in one invoice 
+        or note when selecting them in the Orders To Invoice page.
+        """,
+    'author': "Smart Way Business Solutions",
+    'website': "https://www.smartway.co",
+    'category': 'Accounting',
+    'version': '1.0',
+    'depends': ['base', 'sale'],
+    'data': [
+        'wizard/sale_make_invoice_advance_views.xml',
+    ],
+    'images':  ["static/description/image.png"],
+    'license': "Other proprietary",
+}
