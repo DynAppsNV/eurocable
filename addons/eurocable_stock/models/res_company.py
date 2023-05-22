efea63eda462efd174f20af5d8481424d5c817aa
Keval <keval.mehta@eezee-it.com>
2023-05-22 12:50:00 +0530

    [CHANGE] Add change in ZPL report

--- addons/eurocable_stock/models/res_company.py
+++ addons/eurocable_stock/models/res_company.py
@@ -6,4 +6,6 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    logo_zpl = fields.Text(string="Logo ZPL")
+    logo_zpl = fields.Binary(string="Logo ZPL",
+                             help="This logo is used in ZPL type report "
+                                  "print.")
