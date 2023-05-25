631ff31407beb0986191012ea8de9edefba00dec
Keval Mehta <keval.m.erpharbor@gmail.com>
2023-05-25 14:11:08 +0000

    [FIX] Pep8

--- addons/eurocable_mrp/models/mrp_bom.py
+++ addons/eurocable_mrp/models/mrp_bom.py
@@ -2,7 +2,7 @@
 # Copyright 2023 Eezee-IT (<http://www.eezee-it.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models, api
+from odoo import models, api
 
 
 class MrpBom(models.Model):
