601934bd83730c64c8b94a3a13d87eaeed8a0421
Houssine BAKKALI <houssine.bakkali@gmail.com>
2023-06-21 14:22:38 +0000

    [hba-30421] green label new design

---
+++ addons/eurocable_stock/models/product.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Eezee-IT (<http://www.eezee-it.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from odoo import models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def get_description_sale_zpl(self):
+        description_zpl = self.description_sale
+        if description_zpl:
+            description_zpl = description_zpl.replace('\n', '\&') # NOQA
+        return description_zpl
