f510d56401ffe60f729313cfaaa3f7c59bf0ec56
Schmitz Kurt <kurt.schmitz@dynapps.be>
2024-02-13 12:40:06 +0100

    [ECB-30] Upgrade data V17 --> init xx_all/xx_base module for upgrade/migration scripts

---
+++ addons/xx_all/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Install all custom modules",
+    "version": "15.0.1.0.0",
+    "author": "dynapps",
+    "website": "https://www.dynapps.be",
+    "category": "Hidden",
+    "license": "LGPL-3",
+    "depends": [
+        "xx_base",
+    ],
+    "installable": True,
+}
---
+++ addons/xx_base/__manifest__.py
@@ -0,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Eurocable Base",
+    "version": "15.0.1.0.0",
+    "author": "dynapps",
+    "license": "AGPL-3",
+    "website": "https://www.dynapps.be",
+    "category": "hidden",
+    "depends": [],
+    "data": [],
+    "demo": [],
+    "installable": True,
+}
