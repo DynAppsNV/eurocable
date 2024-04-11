a09c2fba0d7169fa324659d37e2b23f187f20f57
Wodran Van de Sande <wodran.vandesande@dynapps.be>
2024-04-11 14:21:58 +0200

    [ECB-35]RFC02- Samengestelde artikelen configuratie

--- addons/eurocable_mrp/models/__init__.py
+++ addons/eurocable_mrp/models/__init__.py
@@ -1,2 +1,5 @@
 from . import mrp_bom
-
+from . import mrp_production
+from . import sale_order_line
+from . import stock_lot
+from . import stock_move
--- addons/eurocable_sale/models/sale_order_line.py
+++ addons/eurocable_sale/models/sale_order_line.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -7,22 +7,3 @@ class SaleOrderLine(models.Model):
     document_type = fields.Many2one(related="product_id.document_type_id")
     has_certificate = fields.Boolean(default=False)
     certificate_notes = fields.Text()
-    weight = fields.Float(default=0.0)
-    weight_total = fields.Float(default=0.0, compute="_compute_total_weight")
-
-    @api.onchange("product_id")
-    def _compute_weight(self):
-        if self.product_id:
-            self.weight = self.product_id.weight
-
-    @api.depends("product_template_id", "product_id", "product_uom_qty", "weight")
-    def _compute_total_weight(self):
-        for rec in self:
-            rec.weight_total = 0.0
-            if rec.weight:
-                rec.weight_total = rec.weight * rec.product_uom_qty
-
-    def _prepare_invoice_line(self, **optional_values):
-        res = super()._prepare_invoice_line(**optional_values)
-        res.update({"weight": self.weight})
-        return res
