6ce15ac9c956543338dc6ccc3689520b44bc1bba
Xander De Jaegere <xander.dejaegere@dynapps.be>
2024-07-09 12:31:13 +0200

    [ECB-76] [ADD] eurocable_account: pre-commit

--- addons/eurocable_account/migrations/17.0.0.1.3/post-remove_current_default_on_account_to_checked.py
+++ addons/eurocable_account/migrations/17.0.0.1.3/post-remove_current_default_on_account_to_checked.py
@@ -1,7 +1,10 @@
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    # Remove the default value on the field `to_check` of `account.move`, it is not needed anymore
-    env["ir.default"].search([("field_id", "=", env.ref("account.field_account_move__to_check").id)]).unlink()
+    # Remove the default value on the field `to_check` of `account.move`,
+    # it is not needed anymore
+    env["ir.default"].search(
+        [("field_id", "=", env.ref("account.field_account_move__to_check").id)]
+    ).unlink()
