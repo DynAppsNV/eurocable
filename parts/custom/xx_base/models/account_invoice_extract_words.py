60d49322cb280cabf30d463fb7f58f559a194ba2
Robbe Degrève <robbe.degreve@dynapps.be>
2024-12-23 19:17:44 +0100

    Update from dyn-tools

--- parts/custom/xx_base/models/account_invoice_extract_words.py
+++ parts/custom/xx_base/models/account_invoice_extract_words.py
@@ -2,6 +2,6 @@ from odoo import fields, models
 
 
 class AccountInvoiceExtractionWords(models.Model):
-    _inherit = "account_invoice_extract.account.invoice_extract.words"
+    _inherit = "account.invoice_extract.words"
 
     invoice_id = fields.Many2one(required=False)
