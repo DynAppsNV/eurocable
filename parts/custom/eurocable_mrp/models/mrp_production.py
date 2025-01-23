8a4add5ad8d003ffc4af211209ca7a5fc84353e4
Gitlab CI <gitlab-ci@dynapps.be>
2025-01-23 15:35:32 +0000

    Update from dyn-tools

--- parts/custom/eurocable_mrp/models/mrp_production.py
+++ parts/custom/eurocable_mrp/models/mrp_production.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    xx_weight = fields.Float(string="Weight", compute="_compute_total_weight", store=True)
+    xx_weight = fields.Float(string="Weight per Unit", compute="_compute_total_weight", store=True)
     xx_product_description_variants = fields.Text("Custom Description")
 
     @api.depends("move_raw_ids.xx_weight", "origin")
@@ -13,6 +13,7 @@ class MrpProduction(models.Model):
             rec.xx_weight = 0
             for move in rec.move_raw_ids:
                 rec.xx_weight += move.xx_weight
+            rec.xx_weight /= rec.product_qty or 1.0
 
             sale_order = self.env["sale.order"].search([("name", "=", rec.origin)])
             for line in sale_order.order_line:
