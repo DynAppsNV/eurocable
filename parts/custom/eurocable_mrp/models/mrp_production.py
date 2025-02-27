72cac7a3ed6628d5186a41b145ae6ae6ce2219c0
Gitlab CI <gitlab-ci@dynapps.be>
2025-02-27 15:37:36 +0000

    Update from dyn-tools

--- parts/custom/eurocable_mrp/models/mrp_production.py
+++ parts/custom/eurocable_mrp/models/mrp_production.py
@@ -6,6 +6,9 @@ class MrpProduction(models.Model):
 
     xx_weight = fields.Float(string="Weight per Unit", compute="_compute_total_weight", store=True)
     xx_product_description_variants = fields.Text("Custom Description")
+    xx_customer_ids = fields.Many2many(
+        comodel_name="res.partner", compute="_compute_customers", store=True
+    )
 
     @api.depends("move_raw_ids.xx_weight", "origin")
     def _compute_total_weight(self):
@@ -18,3 +21,13 @@ class MrpProduction(models.Model):
             sale_order = self.env["sale.order"].search([("name", "=", rec.origin)])
             for line in sale_order.order_line:
                 line._compute_weight()
+
+    @api.depends("procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id")
+    def _compute_customers(self):
+        for rec in self:
+            partners = rec.mapped(
+                "procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id.partner_id"
+            )
+            rec.xx_customer_ids = partners.browse(
+                [p.id if p.name else p.parent_id.id for p in partners]
+            )
