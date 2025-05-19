3c2e95829a1f6f1715be66a51559c9584a5176f5
Gitlab CI <gitlab-ci@dynapps.be>
2025-05-19 11:13:35 +0000

    Update from dyn-tools

--- parts/eurocable_account/models/account_intrastat_report.py
+++ parts/eurocable_account/models/account_intrastat_report.py
@@ -9,10 +9,17 @@ class IntrastatReportCustomHandler(models.AbstractModel):
     _inherit = "account.intrastat.report.handler"
 
     def _build_query(
-        self, options, column_group_key=None, expanded_line_options=None, offset=0, limit=None
+        self,
+        options,
+        column_group_key=None,
+        expanded_line_options=None,
+        offset=0,
+        limit=None,
+        order_by=True,
+        query_params=None,
     ):
         query, query_params = super()._build_query(
-            options, column_group_key, expanded_line_options, offset, limit
+            options, column_group_key, expanded_line_options, offset, limit, order_by, query_params
         )
         # change query
         for block in filter(lambda b: isinstance(b, SQL), query["select"].seq):
