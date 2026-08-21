5222a17dd0362f3c61354ec1dae31b70d492471e
Gitlab CI <gitlab-ci@dynapps.be>
2026-08-21 08:27:08 +0000

    Update from dyn-tools

--- parts/eurocable_mrp/__manifest__.py
+++ parts/eurocable_mrp/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Eurocable MRP",
-    "version": "17.0.0.2.0",
+    "version": "17.0.0.2.1",
     "author": "dynapps",
     "category": "Mrp",
     "license": "LGPL-3",
---
+++ parts/eurocable_mrp/migrations/17.0.0.2.1/post-migrate.py
@@ -0,0 +1,10 @@
+from odoo import SUPERUSER_ID, api
+
+
+def migrate(cr, version):
+    if not version:
+        return
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    for record in env["mrp.production"].search(["user_id", "=", False]):
+        record.user_id = record.write_uid.id
