09c33d82360355e4a74d1b9bae1b99756a0b79ec
Gitlab CI <gitlab-ci@dynapps.be>
2026-08-24 14:53:07 +0000

    Update from dyn-tools

--- parts/eurocable_mrp/migrations/17.0.0.2.1/post-migrate.py
+++ parts/eurocable_mrp/migrations/17.0.0.2.1/post-migrate.py
@@ -6,5 +6,7 @@ def migrate(cr, version):
         return
     env = api.Environment(cr, SUPERUSER_ID, {})
 
-    for record in env["mrp.production"].search([("user_id", "=", False)]):
+    for record in env["mrp.production"].search(
+        [("user_id", "=", False), ("create_date", ">=", "2026-01-01 00:00:00")]
+    ):
         record.user_id = record.write_uid.id
